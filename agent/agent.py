9cf2ba0e84a4c660773ee31f5fa3635ccaa475ef
onaidra <adriano.puglisi.193@gmail.com>
2021-02-10 21:15:11 +0100

    changes4

--- agent/agent.py
+++ agent/agent.py
@@ -1,6 +1,6 @@
-"""agent.py
-"""
-from agent.interactions import Richieste,Risposte
+"""agent.py"""
+
+from agent.interactions import *
 import re
 import random
 from colorama import init
@@ -8,32 +8,20 @@ from termcolor import colored
 
 botName=colored('Sara','yellow')
 
-
-database =  {"Rosa":4, "Calla":1.5, "Tulipano":3, "Giglio":3, "Gerbera":1.20, "Margherita":1, "Orchidea":7, "Girasole":4, "Viola":2, "Papavero":0.50,"Fiore di Loto":4,
-"Ginestra":0.2, "Crisantemo":0.7, "arancio":0.3,  "Firdaliso":2.5, "Calendula":1, "Lavanda":0.3, "Non ti scordar di me":0.2, "Ortensia":2, "Peonia":1.5, "Mimosa":3,"Giacinti":8,
-"Narcisi":1,"Ibisco":1.3,"Magnolie":1.2,"Pesco":0.3, "Acacia":0.3, "Azalea":1, "Anemone":1, "Gelsomino":0.2, "Erica":0.3,  "Iris":1.5, "Sterlitzia":2,"lilla":1,
-"Zimnia":1.5,"Bocche di Leone":1,"Eucalipto":0.6,"Amaryllis":1.6,"Fresie":1,"Camelia":0.80}
-#print(database["Eucalipto"])
-database2 = {"Valentino": ["mazzo San Valentino","Il mazzo ha 13 rose rosse",50], "Matrimonio" : ["bouquet per il matrimonio","Il bouquet ha rose bianche, tulipani e fiori d'arancio",40], 
-"Comunione" : ["mazzo per comunione","Il bouquet ha tulipani bianchi, calle, rose bianche e alcuni gigli",60], "Funerale" : ["La corona per un funerale","La corona è composta da rose e calle, rigorosamente bianche",200], 
-"Compleanno": ["mazzo per un compleanno","Il mazzo è formato da gerbere e rose gialle o altrimenti un'orchidea",30], "Festa della donna":["mazzo per la festa della donna","Un bellissimo mazzo di mimosa",25],
-"Anniversario":["mazzo per l'anniversario","Il mazzo ha 7 rose rosse",35],"Battesimo":["mazzo per il battesimo","Il bouquet ha tulipani bianchi, calle, rose bianche e alcuni gigli",60],
-"Fidanzamento":["mazzo per un fidanzamento","Il mazzo ha rose e tulipani rigorosamente rossi",40],"Amica":["mazzo per un amica","Il mazzo prevede un girasole con alcuni tulipani gialli",35]   }
-
-database3={'uno':1,'due':2,'tre':3,'quattro':4,'cinque':5,'sei':6,'sette':7,'otto':8,'nome':9,'dieci':10,'undici':11,'dodici':12,'tredici':13,'quattordici':14,
-'quindici':15,'sedici':16,'diciasette':17,'diciotto':18,'diciannove':19,'venti':20,'ventuno':21,'ventidue':22,
-'trenta':30,'quaranta':40,'cinquanta':50,'sessanta':60,'settanta':70,'ottanta':80,'novanta':90,'cento':100 }
 class Agent:
     def __init__(self, speaker, listener):
         self.speaker = speaker
         self.listener = listener
         self.prezzo=0
+        self.lista_=[]
+        self.listaNum_=[]
+
         self.waitForFlowers= False
+        self.trigger=False
 
     def think_man(self, command:str):
         varx=command.upper()
         match=find_match(self,varx)
-
         if match == '2':
             self.waitForFlowers = True
         if match == '4':     #tipologia evento
@@ -41,17 +29,18 @@ class Agent:
             db2_find(self,varx2) 
         if match== '5':
             price_fun(self)
+        if match=='8':
+            self.trigger=True
+            trigger_request(self,command.split())
         if match==False:
             print(botName+colored(": Mi dispiace non abbiamo nulla",'red'))
             self.speaker.speak("Mi dispiace non abbiamo nulla")
 
 
 def find_match(self,command):
-    #print("find_match")
     for k in Richieste:
         for i, interaction in enumerate(Richieste[k]):
             match = re.search(interaction.upper(),command)
-            print(self.waitForFlowers)
             if self.waitForFlowers == True:
                 varx2=command.split()
                 self.waitForFlowers=False
@@ -68,7 +57,6 @@ def find_match(self,command):
 
 
 def db2_find(self,varx2):
-    #print("db_find")
     for v in varx2:
         v=v.capitalize()
         if v in database2.keys():
@@ -80,6 +68,8 @@ def db2_find(self,varx2):
             print(botName+": "+text1)
             self.speaker.speak(text1)
 
+
+
 def price_fun(self):
     if self.prezzo==0:
         print(botName+": Non ho capito di cosa vuole sapere il prezzo")
@@ -89,29 +79,43 @@ def price_fun(self):
         print(botName+": "+pr)
         self.speaker.speak(pr)
 
+
+
+
 def ownFlowers(self,varx2):
-    lista_=[]
-    listaNum_=[]
+
+    if self.trigger==False:
+        self.lista_=[]
+        self.listaNum_=[]
     for v in varx2:
         v=v.capitalize()
         v=v[:-1]
-        for vv in database:
-            if v in vv:
-                lista_.append(vv)
+        if v != '':   
+            for vv in database:
+                if v in vv:
+                    self.lista_.append(vv)
     for v in varx2:
-        v=v.upper()
+        
         if v.isnumeric():
             v=int(v)
-            listaNum_.append(v)
+            self.listaNum_.append(v)
         else:
+            v=v.upper()
             for vv in database3.keys():
                 vx=vv.upper()
                 if v == vx:
-                    listaNum_.append(database3[vv])
+                    self.listaNum_.append(database3[vv])
+    if self.trigger==False:
+        print_list(self)
+    else:
+        return 
+
+
+def print_list(self):
     x=""
-    if len(lista_)==len(listaNum_) and len(lista_)!=0:
-        for elem in range(len(lista_)):
-            x+=lista_[elem]+" "+str(listaNum_[elem])+" "
+    if len(self.lista_)==len(self.listaNum_) and len(self.lista_)!=0:
+        for elem in range(len(self.lista_)):
+            x+=self.lista_[elem]+" "+str(self.listaNum_[elem])+" "
         fun="Perfetto, il suo mazzo sarà composto da: "+x
         print(botName+": "+fun)
         self.speaker.speak(fun)
@@ -122,3 +126,21 @@ def ownFlowers(self,varx2):
         self.waitForFlowers = True
 
 
+def trigger_request(self,varx):
+    ownFlowers(self,varx)
+    if self.listaNum_==[]:
+        print("Quante ne mettiamo rispettivamente?")
+        self.speaker.speak("Quante ne mettiamo rispettivamente?")
+        command,taken=self.listener.listen()
+        ownFlowers(self,command.split())
+        while len(self.listaNum_)!= len(self.lista_):
+            print("Scusi non ho capito quanti,potrebbe ripetere?")
+            self.speaker.speak("Scusi non ho capito quanti,potrebbe ripetere?")
+            command,taken=self.listener.listen()
+            ownFlowers(self,command.split())
+    for i in range(len(self.lista_)):
+        self.prezzo+=database[self.lista_[i]]*self.listaNum_[i]
+    totale="Perfetto,il totale è:",self.prezzo
+    print(botName+totale)
+    
+    self.trigger=False
--- agent/interactions.py
+++ agent/interactions.py
@@ -1,5 +1,20 @@
 """interactions.py
 """
+database =  {"Rosa":4, "Calla":1.5, "Tulipano":3, "Giglio":3, "Gerbera":1.20, "Margherita":1, "Orchidea":7, "Girasole":4, "Viola":2, "Papavero":0.50,"Fiore di Loto":4,
+"Ginestra":0.2, "Crisantemo":0.7, "arancio":0.3,  "Firdaliso":2.5, "Calendula":1, "Lavanda":0.3, "Non ti scordar di me":0.2, "Ortensia":2, "Peonia":1.5, "Mimosa":3,"Giacinti":8,
+"Narcisi":1,"Ibisco":1.3,"Magnolie":1.2,"Pesco":0.3, "Acacia":0.3, "Azalea":1, "Anemone":1, "Gelsomino":0.2, "Erica":0.3,  "Iris":1.5, "Sterlitzia":2,"lilla":1,
+"Zimnia":1.5,"Bocche di Leone":1,"Eucalipto":0.6,"Amaryllis":1.6,"Fresie":1,"Camelia":0.80}
+#print(database["Eucalipto"])
+database2 = {"Valentino": ["mazzo San Valentino","Il mazzo ha 13 rose rosse",50], "Matrimonio" : ["bouquet per il matrimonio","Il bouquet ha rose bianche, tulipani e fiori d'arancio",40], 
+"Comunione" : ["mazzo per comunione","Il bouquet ha tulipani bianchi, calle, rose bianche e alcuni gigli",60], "Funerale" : ["La corona per un funerale","La corona è composta da rose e calle, rigorosamente bianche",200], 
+"Compleanno": ["mazzo per un compleanno","Il mazzo è formato da gerbere e rose gialle o altrimenti un'orchidea",30], "Festa della donna":["mazzo per la festa della donna","Un bellissimo mazzo di mimosa",25],
+"Anniversario":["mazzo per l'anniversario","Il mazzo ha 7 rose rosse",35],"Battesimo":["mazzo per il battesimo","Il bouquet ha tulipani bianchi, calle, rose bianche e alcuni gigli",60],
+"Fidanzamento":["mazzo per un fidanzamento","Il mazzo ha rose e tulipani rigorosamente rossi",40],"Amica":["mazzo per un amica","Il mazzo prevede un girasole con alcuni tulipani gialli",35]   }
+
+database3={'uno':1,'due':2,'tre':3,'quattro':4,'cinque':5,'sei':6,'sette':7,'otto':8,'nome':9,'dieci':10,'undici':11,'dodici':12,'tredici':13,'quattordici':14,
+'quindici':15,'sedici':16,'diciasette':17,'diciotto':18,'diciannove':19,'venti':20,'ventuno':21,'ventidue':22,
+'trenta':30,'quaranta':40,'cinquanta':50,'sessanta':60,'settanta':70,'ottanta':80,'novanta':90,'cento':100 }
+
 
 Richieste = {
     '1': ["([a-z])?(Mi servirebbe|Vorrei|Ho bisogno di|Desidero|Voglio|Mi serve|Vorrei comprare|Voglio comprare|Desidero comprare)? un mazzo di fiori([a-z])?"],
@@ -9,6 +24,7 @@ Richieste = {
     '5': ["([a-z])?(prezzo|costa|viene|costo|a quanto)([a-z])?"],
     '6': ["([a-z])?(lo prendo|va bene|perfetto|ok)([a-z])?"],
     '7': ["([a-z])?(altri|altro|diverso|diversi)([a-z])?"],
+    '8': ["([a-z])?(mazzo di|mazzetto di)([a-z])?"],
 
     #'saluto': ["([a-z])?(arrivederci|ciao|alla prossima)([a-z])?"]
 
@@ -22,6 +38,7 @@ Risposte = {
     '5': [""],
     '6': ["Ecco a lei il mazzo, arrivederla"],
     '7': ["No mi spiace, è l'unico che abbiamo per quel tipo di occasione"],
+    '8': [""],
     'fiori':[]
     #'saluto': ["Arrivederci"]
 }
